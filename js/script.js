669a10a7b4fcd54feddded0b42cb51313d274f2b
Cedrik6245653 <6245653@cegepmontpetit.ca>
2025-03-05 17:57:22 -0500

    commit remise

--- js/script.js
+++ js/script.js
@@ -1,24 +1,16 @@
-const boutonsModale = document.querySelectorAll('.bouton');
+const boutonsModale = document.querySelectorAll('.boutonModale');
 const modale = document.querySelector('.modale');
+
+modale.addEventListener("click", closeModale);
 
 for (let i = 0; i < boutonsModale.length; i++) {
     boutonsModale[i].addEventListener('click', openModale);
 }
 
 function openModale(event) {
-    const chemin = event.currentTarget.previousElementSibling.getAttribute("src");
-    
-    const image = document.createElement('img');
-    image.setAttribute('src', chemin);
-    image.classList.add('zoom');
-
-    modale.appendChild(image);
-    modale.classList.add('visible');
-
-    image.addEventListener("click", closeModale);
+    modale.classList.add('visible');    
 }
 
 function closeModale(event) {
-    modale.removeChild(event.currentTarget);
     modale.classList.remove('visible');
 }
